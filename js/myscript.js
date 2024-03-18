8c17831aac6652d12a03a6e7a629ca36b9369576
Nik0-9 <filannino.nicola9@gmail.com>
2024-03-18 16:28:55 +0100

    does't work

--- js/myscript.js
+++ js/myscript.js
@@ -10,17 +10,18 @@ btnStart.addEventListener('click', function () {
     mainEl.appendChild(sectionEl);
     const gridEl = document.getElementById('grid');
     gridEl.classList.add('d-flex', 'flex-wrap');
-    play(difficulty, gridEl);
     let listBomb = [];
     numBomb = 16;
     for(i=0; i<numBomb; i++){
         listBomb = getRandomIntUnique(1, 100, numBomb);
     }
+    play(difficulty, gridEl, listBomb);
     
     //console.log(list);
 });
 
 function getCellsByDifficulty(difficulty){
+    
     if(difficulty === 'easy'){
         return 100;
     }else if (difficulty === 'medium'){
@@ -33,7 +34,7 @@ function getCellsByDifficulty(difficulty){
 function generateCell(content, difficulty) {
 
     let addCell = document.createElement('div');
-    addCell.innerHTML = content;
+    addCell.innerText = content;
     addCell.classList.add('cell');
     if(difficulty !== 'easy'){
         addCell.classList.add(`cell-${difficulty}`);
@@ -41,14 +42,22 @@ function generateCell(content, difficulty) {
     return addCell;
 }
 
-function play(difficulty, grid){
+function play(difficulty, grid, listBomb){
+    console.log(listBomb);
     const cells = getCellsByDifficulty(difficulty);
     for (i = 0; i < cells ; i++){
         const newCell = generateCell(i + 1, difficulty);
         grid.appendChild(newCell);
         newCell.addEventListener('click', function () {
+            console.log(newCell.innerText);
+
+           if(listBomb.includes(newCell.innerText)){
+            console.log(newCell.innerText);
+
+            newCell.classList.add('bomb');
+           }else{
             newCell.classList.add('clicked');
-            console.log(newCell.innerHTML);
+           }
         });
     }
 }

36bd0a9100845ce1da6b7bd61d4316196fead7c8
Nik0-9 <filannino.nicola9@gmail.com>
2024-03-18 17:57:39 +0100

    added condition in newCell click function that control the value of isFound var and if user click on a cell that contain bomb isFound is true and gameover

--- js/myscript.js
+++ js/myscript.js
@@ -1,32 +1,38 @@
 let btnStart = document.querySelector('.btn-outline-dark');
 
 btnStart.addEventListener('click', function () {
-    
+
     let difficulty = document.getElementById('difficulty').value;
     const mainEl = document.querySelector('main');
     const sectionEl = document.createElement('section');
-    sectionEl.id ='grid';
-    mainEl.innerHTML ='';
+    sectionEl.setAttribute('id', 'grid');
+    createDiv = document.createElement('div');
+    createDiv.setAttribute('id', 'risultato');
+    createDiv.classList.add('result');
+    createDiv.innerHTML = '';
+    mainEl.innerHTML = '';
     mainEl.appendChild(sectionEl);
+    mainEl.appendChild(createDiv);
     const gridEl = document.getElementById('grid');
     gridEl.classList.add('d-flex', 'flex-wrap');
     let listBomb = [];
     numBomb = 16;
-    for(i=0; i<numBomb; i++){
-        listBomb = getRandomIntUnique(1, 100, numBomb);
-    }
-    play(difficulty, gridEl, listBomb);
     
+        for (i = 0; i < numBomb; i++) {
+            listBomb = getRandomIntUnique(1, 100, numBomb);
+        }
+        play(difficulty, gridEl, listBomb);
+
     //console.log(list);
 });
 
-function getCellsByDifficulty(difficulty){
-    
-    if(difficulty === 'easy'){
+function getCellsByDifficulty(difficulty) {
+
+    if (difficulty === 'easy') {
         return 100;
-    }else if (difficulty === 'medium'){
+    } else if (difficulty === 'medium') {
         return 81;
-    }else if(difficulty === 'hard'){
+    } else if (difficulty === 'hard') {
         return 49;
     }
 }
@@ -36,27 +42,38 @@ function generateCell(content, difficulty) {
     let addCell = document.createElement('div');
     addCell.innerText = content;
     addCell.classList.add('cell');
-    if(difficulty !== 'easy'){
+    if (difficulty !== 'easy') {
         addCell.classList.add(`cell-${difficulty}`);
     }
     return addCell;
 }
 
-function play(difficulty, grid, listBomb){
+function play(difficulty, grid, listBomb) {
     console.log(listBomb);
     const cells = getCellsByDifficulty(difficulty);
-    for (i = 0; i < cells ; i++){
+    let score = 0;
+    let isFound = false;
+    for (i = 0; i < cells; i++) {
         const newCell = generateCell(i + 1, difficulty);
         grid.appendChild(newCell);
         newCell.addEventListener('click', function () {
-            console.log(newCell.innerText);
-            let isFound = false;
-           if(listBomb.includes(parseInt(newCell.innerText))){
-            isFound = true;
-           }
-           if(isFound){
-            
-           }
+            if (!isFound) {
+                console.log(newCell.innerText);
+                if (listBomb.includes(parseInt(newCell.innerText))) {
+                    isFound = true;
+                }
+                if (isFound) {
+                    newCell.classList.add('bomb');
+                } else {
+                    if (!newCell.classList.contains('clicked')) {
+                        score++;
+                        newCell.classList.add('clicked');
+                    }
+                }
+                createDiv.innerHTML = `Il tuo punteggio è: ${score}`;
+            } else {
+                createDiv.innerHTML = `Hai perso il tuo punteggio è: ${score}`;
+            }
         });
     }
 }
